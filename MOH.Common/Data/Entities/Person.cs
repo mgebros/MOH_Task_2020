6e9dd105905998a9e1742b3402936c29f30692d6
Giorgi Mgebrishvili <georgem94@outlook.com>
2020-05-20 01:01:05 +0400

    Add project files.

---
+++ MOH.Common/Data/Entities/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOH.Common.Data.Entities
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string PrivateNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Phone { get; set; }
+        public int Profession { get; set; }
+    }
+
+
+    public enum Professions
+    {
+
+    }
+
+
+
+}
